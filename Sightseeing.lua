d49c12b74c89aa863de6a9a07af2f7853751d8e1
Koen Kamman <kammankoen@gmail.com>
2017-11-17 19:25:09 +0100

    Added interface options panel.

--- Sightseeing.lua
+++ Sightseeing.lua
@@ -4,6 +4,8 @@ EventFrame:SetScript("OnEvent",
 
 	function(self,event,...)
 
+		-- Add Circle to Worldmap
+
 		local Frame = CreateFrame("Frame", "Overlay", WorldMapDetailFrame);
 		Frame:SetAllPoints();
 		Frame:SetFrameStrata("FULLSCREEN_DIALOG");
@@ -17,6 +19,29 @@ EventFrame:SetScript("OnEvent",
 
 		Frame.Texture:SetPoint("TOPLEFT", WorldMapDetailFrame, (width*unitX)-textureSize/2, (-height*unitY)+textureSize/2);
 		Frame.Texture:SetTexture("Interface\\Minimap\\UI-QuestBlob-MinimapRing");
+
+		
+		-- Add Interface Options
+
+		local AddonOptions = {};
+		AddonOptions.mainPanel = CreateFrame( "Frame", "Sightseeing", UIParent );
+		AddonOptions.mainPanel.name = "Sightseeing";
+
+		InterfaceOptions_AddCategory(AddonOptions.mainPanel);
+		
+		AddonOptions.viewpointPanel = CreateFrame( "Frame", "SightseeingViewpoint", AddonOptions.mainPanel);
+		AddonOptions.viewpointPanel.name = "Viewpoint";
+		AddonOptions.viewpointPanel.parent = AddonOptions.mainPanel.name;
+
+		InterfaceOptions_AddCategory(AddonOptions.viewpointPanel);
+
+
+		-- Register config cmd
+
+		SLASH_CONFIG1 = "/sightseeing"
+		SlashCmdList["CONFIG"] = function(self, txt)
+			InterfaceOptionsFrame_OpenToCategory(AddonOptions.mainPanel.name);
+		end
 
 	end
 
